8c5ae465c8f71b77e1058930bc690c4eed41a3e8
Sorah Fukumori <sorah@cookpad.com>
2018-08-14 09:01:28 +0900

    AdminLTE

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'webpacker'
 
+gem 'haml', '>= 5.0.0'
+
 gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,13 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import 'bootstrap/dist/css/bootstrap';
+import 'admin-lte/dist/css/AdminLTE';
+import 'admin-lte/dist/css/skins/skin-red';
+
+import 'font-awesome/css/font-awesome';
+
+import 'bootstrap/dist/js/bootstrap';
+import 'admin-lte/dist/js/adminlte';
+
+
