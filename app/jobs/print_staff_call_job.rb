375468f65608232a8c38e0222b59eb7d45278b0c
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-08-28 00:10:50 +0900

    PrintStaffCallJob

---
+++ app/jobs/print_staff_call_job.rb
@@ -0,0 +1,18 @@
+require 'ioiprint'
+
+class PrintStaffCallJob < ApplicationJob
+  def perform(contestant:, message:)
+    Ioiprint.new.print_staff_call(
+      message: message,
+      contestant: {
+        id: contestant.login,
+        name: contestant.name,
+        special_requirement_note: contestant.special_requirement_note.presence,
+      },
+      desk: {
+        id: contestant.desk&.name || 'N/A',
+        map: nil,  # TODO!!!
+      },
+    )
+  end
+end
--- lib/ioiprint.rb
+++ lib/ioiprint.rb
@@ -1,5 +1,3 @@
-require 'uri'
-
 class Ioiprint
   def initialize(url = ENV.fetch('IOIPRINT_URL'))
     @conn = Faraday.new(url: url) do |faraday|
@@ -16,5 +14,12 @@ class Ioiprint
   # users:: [{name: String, username: String, password: String}]
   def print_passwords(title:, users:)
     conn.post '/password', {title: title, users: users}
+  end
+
+  # message:: String
+  # contestant:: {id: String, name: String, special_requirement_note: String?}
+  # desk:: {id: String, map: URI?, zone: String?}
+  def print_staff_call(message:, contestant:, desk:)
+    conn.post '/staff_call', {message: message, contestant: contestant, desk: desk}
   end
 end
