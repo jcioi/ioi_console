909d4a978549930ee3705f7846ccf27c057e5af6
Sorah Fukumori <sorah@cookpad.com>
2018-09-01 22:27:34 +0900

    ImportMachinesFromAllSwitchesJob: perform(now: true)

--- app/jobs/import_machines_from_all_switches_job.rb
+++ app/jobs/import_machines_from_all_switches_job.rb
@@ -1,7 +1,11 @@
 class ImportMachinesFromAllSwitchesJob < ApplicationJob
-  def perform()
+  def perform(now: false)
     Rails.application.config.x.ipam.switch_hosts.each do |host|
-      FetchMachinesFromSwitchJob.perform_later(host)
+      if now
+        FetchMachinesFromSwitchJob.perform_now(host)
+      else
+        FetchMachinesFromSwitchJob.perform_later(host)
+      end
     end
   end
 end
